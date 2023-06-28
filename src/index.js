9e7ebc5f5b8a07699901ff36978350facb7974ee
nickstuffz <112445360+nickstuffz@users.noreply.github.com>
2023-06-28 14:33:17 -0700

    Complete loadHeader function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
-console.log("Potato inside index.js");
+// import page modules
+
+
+const tab_home = document.getElementById('tab-home');
+const tab_menu = document.getElementById('tab-menu');
+const tab_contact = document.getElementById('tab-contact');
+
+tab_home.addEventListener('click', () => {
+    clearPage();
+    loadHeader();
+    loadHome();
+    return;
+});
+
+tab_menu.addEventListener('click', () => {
+    clearPage();
+    loadHeader();
+    loadMenu();
+    return;
+});
+
+tab_contact.addEventListener('click', () => {
+    clearPage();
+    loadHeader();
+    loadContact();
+    return;
+});
+
+function loadHeader() {
+    // loads header
+}
+
+function clearPage() {
+    // clears page
+}
+
+loadHome();
